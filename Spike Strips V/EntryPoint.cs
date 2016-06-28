cea535eb08c6a5e1a2d2ee70919649d9b795dc78
alexguirre <alexguirre.aa@gmail.com>
2016-06-28 21:35:16 +0200

    Clean up code

---
+++ Spike Strips V/EntryPoint.cs
@@ -0,0 +1,54 @@
+﻿namespace Spike_Strips_V
+{
+    // RPH
+    using Rage;
+
+    internal static class EntryPoint
+    {
+        public static int NumberOfStingersToSpawn = 1;
+
+        public static StaticFinalizer Finalizer;
+
+        public static void Main()
+        {
+            while (Game.IsLoading)
+                GameFiber.Yield();
+
+            Logger.LogWelcome();
+
+            Finalizer = new StaticFinalizer(delegate { StingersPool.DeleteAllStingers(); });
+
+            StingersPool.Initalize();
+
+            while (true)
+            {
+                GameFiber.Yield();
+
+                //** CHANGE SIZE
+                if (Control.Increase.IsJustPressed())
+                {
+                    NumberOfStingersToSpawn = MathHelper.Clamp(NumberOfStingersToSpawn + 1, 1, 7);
+                    if (NumberOfStingersToSpawn == 7)
+                        NumberOfStingersToSpawn = 1;
+                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + NumberOfStingersToSpawn.ToString(), 1000);
+                }
+                else if (Control.Decrease.IsJustPressed())
+                {
+                    NumberOfStingersToSpawn = MathHelper.Clamp(NumberOfStingersToSpawn - 1, 0, 6);
+                    if (NumberOfStingersToSpawn == 0)
+                        NumberOfStingersToSpawn = 6;
+                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + NumberOfStingersToSpawn.ToString(), 1000);
+                }
+
+                if (Control.Deploy.IsJustPressed() && !Game.LocalPlayer.Character.IsInAnyVehicle(false) && !Game.LocalPlayer.Character.IsInCover && !Game.LocalPlayer.Character.IsJumping)
+                {
+                    StingersPool.CreateStingers(NumberOfStingersToSpawn);
+                }
+                else if (Control.Remove.IsJustPressed())
+                {
+                    StingersPool.DeleteAllStingers();
+                }
+            }
+        }
+    }
+}
