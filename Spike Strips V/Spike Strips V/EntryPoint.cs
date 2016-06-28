56c7a42219b7a24e7ea30fd002ac6f19f931a918
alexguirre <alexguirre.aa@gmail.com>
2016-06-28 21:36:26 +0200

    Clean up code

--- Spike Strips V/Spike Strips V/EntryPoint.cs
+++ Spike Strips V/Spike Strips V/EntryPoint.cs
@@ -1,12 +1,11 @@
 ﻿namespace Spike_Strips_V
 {
+    // RPH
     using Rage;
 
     internal static class EntryPoint
     {
-        public static int numSize = 1;
-        public static int numStinger = 1;
-        public static int numAddStinger = 1;
+        public static int NumberOfStingersToSpawn = 1;
 
         public static StaticFinalizer Finalizer;
 
@@ -20,7 +19,6 @@
             Finalizer = new StaticFinalizer(delegate { StingersPool.DeleteAllStingers(); });
 
             StingersPool.Initalize();
-            Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + numSize.ToString(), 1500);
 
             while (true)
             {
@@ -29,46 +27,26 @@
                 //** CHANGE SIZE
                 if (Control.Increase.IsJustPressed())
                 {
-                    numStinger += numAddStinger;
-                    numSize++;
-                    if (numSize < 7)
-                    {
-                        Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + numSize.ToString(), 1000);
-                    }
+                    NumberOfStingersToSpawn = MathHelper.Clamp(NumberOfStingersToSpawn + 1, 1, 7);
+                    if (NumberOfStingersToSpawn == 7)
+                        NumberOfStingersToSpawn = 1;
+                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + NumberOfStingersToSpawn.ToString(), 1000);
                 }
                 else if (Control.Decrease.IsJustPressed())
                 {
-                    numStinger -= numAddStinger;
-                    numSize--;
-                    if (numSize < 7)
-                    {
-                        Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + numSize.ToString(), 1000);
-                    }
+                    NumberOfStingersToSpawn = MathHelper.Clamp(NumberOfStingersToSpawn - 1, 0, 6);
+                    if (NumberOfStingersToSpawn == 0)
+                        NumberOfStingersToSpawn = 6;
+                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + NumberOfStingersToSpawn.ToString(), 1000);
                 }
 
                 if (Control.Deploy.IsJustPressed() && !Game.LocalPlayer.Character.IsInAnyVehicle(false) && !Game.LocalPlayer.Character.IsInCover && !Game.LocalPlayer.Character.IsJumping)
                 {
-                    StingersPool.CreateStingers(numStinger);
+                    StingersPool.CreateStingers(NumberOfStingersToSpawn);
                 }
                 else if (Control.Remove.IsJustPressed())
                 {
                     StingersPool.DeleteAllStingers();
-                }
-
-
-                if (numSize == 7)
-                {
-                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + "1", 1000);
-                    //numCaltrops = 16;
-                    numStinger = 1;
-                    numSize = 1;
-                }
-                else if (numSize == 0)
-                {
-                    Game.DisplaySubtitle("~r~Spike Strips ~n~~b~Size: " + "6", 1000);
-                    //numCaltrops = 106;
-                    numStinger = 6;
-                    numSize = 6;
                 }
             }
         }
